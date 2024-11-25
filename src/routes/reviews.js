fbe9d914e555ea692dccb0282998e9a8a5b96221
Emmanuel Arceo <133131186+TheSCodex@users.noreply.github.com>
2024-11-24 19:02:40 -0500

    rutas de reseñas

---
+++ src/routes/reviews.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const reviewsController = require('../controllers/reviews');
+
+const reviewsRoutes = express.Router();
+
+reviewsRoutes.get('/reviews/', reviewsController.getAllReviews);
+reviewsRoutes.get('/reviews/:id', reviewsController.getReviewById);
+reviewsRoutes.post('/reviews/', reviewsController.createReview);
+reviewsRoutes.put('/reviews/:id', reviewsController.updateReview);
+reviewsRoutes.delete('/reviews/:id', reviewsController.deleteReview);
+reviewsRoutes.get('/reviews/restaurant/:restaurantId', reviewsController.getReviewsByRestaurantId);
+
+module.exports = reviewsRoutes;
